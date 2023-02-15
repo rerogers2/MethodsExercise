5af14f241dd115dece5863ea55bd3710c4559811
Robert Rogers <rerogers@gmail.com>
2023-02-15 15:10:15 -0600

    updated the exercise to add some math methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,8 +1,29 @@
-﻿namespace MethodsExercise
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Schema;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
+        {
+            story(); // writes a story based on inputs
+
+            // finding the hypotenuse of a right triangle given two sides 
+            Console.WriteLine("Let's find the hypotenuse of a right triangle.\n");
+            Console.Write("Side length one: "); // output for side one
+            var num1 = Convert.ToDouble(Console.ReadLine()); // input for side one
+            Console.Write("Side length two: "); // output for side two
+            var num2 = Convert.ToDouble(Console.ReadLine()); // input for side two
+
+            Console.WriteLine($"\nThe hypotenuse of your right triangle with side lengths {num1} and {num2} is {hyp(num1, num2)}.\n"); // finds the hypotenuse of a right triangle
+            Console.WriteLine($"The longest hypotenuse length those two sides could create must be less than {maxlength(num1, num2)}.\n"); // finds the max possible length
+            Console.WriteLine($"The shortest hypotenuse length those two sides could create must be larger than {minlength(num1, num2)}.\n"); // finds the min possible length
+            Console.WriteLine($"The sum of 5 and 12 is {sum(5, 12)}.\n");
+        }
+
+        public static void story()
         {
             Console.WriteLine("What is your name?"); // output for name
             var name = Console.ReadLine(); // input for name
@@ -14,11 +35,34 @@
             var team = Console.ReadLine(); // input for baseball team
             string? teamout; // creates a null variable for teamout
             if (team != "Astros")
-                teamout = $"I hope the {team} lose to the Astros in the World Series.";
+                teamout = $"I hope the {team} miss the playoffs. GO ASTROS!";
             else
                 teamout = $"I hope the {team} win the World Series!";
             Console.WriteLine($"Hello, {name}! I'm surprised that someone who is {age} years old considers {color} to be their favorite color. Good for you. \n" +
-                $"{teamout}");
+                $"{teamout}\n");
+        }
+
+        public static double hyp(double num1, double num2)
+        {
+            double total = Math.Round(Math.Sqrt(Math.Pow(num1, 2) + Math.Pow(num2, 2)),2);
+            return total;
+        }
+
+        public static double maxlength(double num1, double num2)
+        {
+            double total = Math.Round((num1 + num2),2);
+            return total;
+        }
+
+        public static double minlength(double num1, double num2) 
+        {
+            double total = Math.Round(Math.Abs(num1 - num2),2);
+            return total;
+        }
+
+        public static int sum(int a, int b)
+        {
+            return a + b; 
         }
     }
 }
